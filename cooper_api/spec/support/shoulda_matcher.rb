8c854c911a6b84df4d8135bd018269b28f1fb798
Jesper <jesper_gs_@hotmail.com>
2016-09-23 02:47:44 +0200

    fixed syntax error in spec/support/shoulda_matcher.rb

--- cooper_api/spec/support/shoulda_matcher.rb
+++ cooper_api/spec/support/shoulda_matcher.rb
@@ -2,7 +2,7 @@ Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
-  endend
+  end
 
 RSpec.configure do |config|
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
